6e0f8ebad6048ff881e093afd9405accf72a5c07
Austin Rose <austin.rose94@gmail.com>
2016-11-16 22:36:14 -0500

    Mmm

--- app.js
+++ app.js
@@ -6,45 +6,35 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var pg = require('pg');
 
 // Local JS code.
 var routes = require('./routes/index');
-var users = require('./routes/users');
 
 // Set up objects.
 var app = express();
-var connection_string = (process.env.DATABASE_URL || 'postgres://localhost:5432/webware');
-var client = new pg.Client(connection_string);
 
 // View engine setup.
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-// UnconnectionStringcomment after placing your favicon in /public
+// Conf.
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-
 app.use(cookieSession({name: 'session', keys: ['key1', 'key2']}));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
 app.use('/', routes);
-app.use('/users', users);
 
-// Catch 404 and forward to error handler
+// Catch 404 and forward to error handler.
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
-// error handlers
-
-// development error handler
-// will print stacktrace
+// Development error handler will print stacktrace.
 if (app.get('env') === 'development') {
   app.use(function(err, req, res, next) {
     res.status(err.status || 500);
@@ -55,8 +45,7 @@ if (app.get('env') === 'development') {
   });
 }
 
-// production error handler
-// no stacktraces leaked to user
+// Production error handler no stacktraces leaked to user.
 app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error', {
@@ -64,6 +53,5 @@ app.use(function(err, req, res, next) {
     error: {}
   });
 });
-
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,106 @@
 var express = require('express');
+var pg = require('pg');
 var router = express.Router();
+var connection_string = (process.env.DATABASE_URL || 'postgres://localhost:5432/webware');
+
+// Creates a student entry with the given name, if it doesn't already exist.
+function create_student(name) {
+  var qs = "INSERT INTO students(name) SELECT ($1) WHERE NOT EXISTS " +
+            "(SELECT name FROM students WHERE name=$1);";
+  var data = [name];
+
+  pg.connect(connection_string, function (err, client, done) {
+    // Handle connection errors
+    if(err) {
+      done();
+      console.log(err);
+      return res.status(500).json({success: false, data: err});
+    }
+
+    // Insert the new student.
+    var query = client.query(qs, data);
+
+    query.on('end', function() {
+      done();
+      return true;
+    });
+  });
+}
+
+// function create_student(name) {
+//   var results = [];
+//   var query = client.query(qs, [name]);
+//
+//   query.on('end', function() {
+//     client.end();
+//   });
+// }
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  // Update views
-  req.session.views = (req.session.views || 0) + 1
+  var role = req.session.role || false;
+  var name = req.session.name || false;
 
-  // Write response
-  res.end(req.session.views + ' views')
+  // Already logged in.
+  if (role && name) {
 
-  // res.render('index', { title: 'Express' });
+    // Redirect to professor page.
+    if (role == "professor") {
+      res.redirect('/professor');
+
+    // Redirect to student page.
+    } else if (role == "student") {
+      res.redirect('/student');
+    }
+
+  // Not already logged in.
+  } else {
+    res.render('login', {logged_in: false});
+  }
+});
+
+router.get('/professor', function (req, res, next) {
+  var role = req.session.role || false;
+  var name = req.session.name || false;
+
+  if (!(role && name) || (role != 'professor')) {
+    res.redirect('/');
+  }
+
+  res.render('professor', {logged_in: true});
+});
+
+router.get('/student', function (req, res, next) {
+  var role = req.session.role || false;
+  var name = req.session.name || false;
+
+  if (!(role && name) || (role != 'student')) {
+    res.redirect('/');
+  }
+
+  // Create student entry if it doesn't exist.
+  create_student(name);
+
+  res.render('student', {logged_in: true});
+});
+
+router.get('/logout', function (req, res, next) {
+  req.session.role = false;
+  req.session.name = false;
+  res.redirect('/');
+});
+
+router.post('/login', function(req, res, next) {
+  var role = req.body.role || false;
+  var name = req.body.name || false;
+
+  if (role && name) {
+    req.session.role = role;
+    req.session.name = name;
+  }
+
+  res.redirect('/');
 });
 
 module.exports = router;
+
